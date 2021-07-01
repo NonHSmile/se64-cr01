1771319389020da6e4275bd9ad09372517b1956e
NonHSmile <apiwatsukthaworn@gmail.com>
2021-07-02 00:04:25 +0700

    extracted CourseItem component

--- cr-frontend/src/App.tsx
+++ cr-frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import { maxHeaderSize } from 'http';
 import React, { useEffect, useState } from 'react';
-
+import { Course } from './interfaces';
 import './App.css';
+import CourseItem from './CourseItem';
  
 
 const App = () => {
-  const [courses, setCourses] = useState<any[]>([]);
+  const [courses, setCourses] = useState<Course[]>([]);
 
   useEffect(() => {
     fetch ('http://localhost:3000/courses')
     .then(res => res.json())
-    .then(courses => {
+    .then(courses =>  {
       console.log(courses);
       setCourses(courses);
     });
@@ -21,39 +21,12 @@ const App = () => {
     <div className="App"> 
       <ul>
         {courses.map(item => (
-          <li key={item.id}>{item.number} - {item.title}</li>
+          <CourseItem key={item.id} course={item} />
         ))}
       </ul>
     </div>
   );
 } 
 
-/*
-type AppState = {
-  message : string; 
-};
-
-class App extends React.Component<{} , AppState> {
-  state: AppState = {
-    message : 'Default message',
-  };
-
-  componentDidMount() {
-    fetch ('http://localhost:3000/courses')
-      .then(res => res.json())
-      .then(obj => {
-        this.setState({message: obj.message});
-      });
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.message}
-      </div>
-    );
-  }
-}
-*/
 
 export default App;
