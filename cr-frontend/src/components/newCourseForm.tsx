85864ab11869902717b4a2a215304b913850ba0e
NonHSmile <apiwatsukthaworn@gmail.com>
2021-07-07 17:54:28 +0700

    handles and validates form inputs using Formik

--- cr-frontend/src/components/newCourseForm.tsx
+++ cr-frontend/src/components/newCourseForm.tsx
@@ -1,42 +1,65 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
 import { Course } from '../interfaces';
 import CoursesService from '../services/CoursesService';
+
 
 type NewCourseFormProps = {
     onNewCourseCreated?: (newCourse: Course) => void,
 };
 
 const NewCourseForm = (props: NewCourseFormProps) => {
-    const [newCourseNumber, setNewCourseNumber] = useState<string>('');
-    const [newCourseTitle, setNewCourseTitle] = useState<string>('');
-
-    const handleNewCourseNumberChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-        setNewCourseNumber(e.target.value);
-      };
-    
-    const handleSave = () =>{
-        const newCourse = {
-            number : newCourseNumber,
-            title : newCourseTitle
-        }
-
-        CoursesService.createCourses(newCourse)
-        .then(savedNewCourse =>  {
-            if(savedNewCourse !== null) {
-                if(props.onNewCourseCreated !== undefined){
-                    props.onNewCourseCreated(savedNewCourse);
-                }
-            } else{
-                alert("Save error");
-            }
-        });   
-    };
     
     return (
         <div>
-            Number: <input value={newCourseNumber} onChange={handleNewCourseNumberChange} /><br />
-            Title: <input value={newCourseTitle} onChange={(e) => {setNewCourseTitle(e.target.value);}} /><br />
-            <button onClick={handleSave}>Save</button>
+            <Formik
+                initialValues={{ newCourseNumber: '' , newCourseTitle: '' }}
+                validate={values => {
+                    const errors:any = {};
+                    
+                    if(values.newCourseTitle === ''){
+                        errors.newCourseTitle = 'Course title is required.'
+                    }
+                    if(values.newCourseNumber === ''){
+                        errors.newCourseNumber = 'Course number is required.'
+                    }else if(!/^[0-9]+$/.test(values.newCourseNumber)){
+                        errors.newCourseNumber = 'Course number format error.'
+                    }
+
+                    return errors;
+                }}
+                onSubmit={(values, actions) => {
+                    const newCourse = {
+                        number : values.newCourseNumber,
+                        title : values.newCourseTitle
+                    }
+            
+                    CoursesService.createCourses(newCourse)
+                    .then(savedNewCourse =>  {
+                        if(savedNewCourse !== null) {
+                            if(props.onNewCourseCreated !== undefined){
+                                props.onNewCourseCreated(savedNewCourse);
+                            }
+                        } else{
+                            alert("Save error");
+                        }
+                        actions.setSubmitting(false);
+                    });   
+                }}
+            >
+                {( {isSubmitting }) => (
+                    <Form>
+                        Number: <Field type="input" name="newCourseNumber"/>
+                        <ErrorMessage name="newCourseNumber" component="div" />
+                        <br />
+                        Title: <Field type="input" name="newCourseTitle"/>
+                        <ErrorMessage name="newCourseTitle" component="div" />
+                        <br />
+                        <button disabled={isSubmitting}>Save</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 };
