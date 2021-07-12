b9e9198ca9652276418952d57fef58f86568407d
NonHSmile <apiwatsukthaworn@gmail.com>
2021-07-13 00:22:51 +0700

    Authentication (back-end) ep 17

--- cr-backend/src/app.module.ts
+++ cr-backend/src/app.module.ts
@@ -7,6 +7,8 @@ import Course from './courses/course.entity';
 import Review from './courses/review.entity';
 
 import { CoursesModule } from './courses/courses.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 
 
@@ -21,6 +23,10 @@ import { CoursesModule } from './courses/courses.module';
     }),
 
     CoursesModule,
+
+    UsersModule,
+
+    AuthModule,
   ],
   controllers: [AppController ],
   providers: [AppService],
